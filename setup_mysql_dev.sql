d0cff803c512f35c02289904b217248d7bb4839e
Zeezbaba <Azeez_okeleji>
2024-02-21 12:57:13 -0800

    database privilege

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+--script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--granting all privileges to user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+--hbnb_dev should have SELECT privilege on the database performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
