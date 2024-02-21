205cb7fda021ac8403182143f598142e028df305
Zeezbaba <Azeez_okeleji>
2024-02-21 13:23:23 -0800

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 --script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS;
+--The password of hbnb_dev should be set to hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --granting all privileges to user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
