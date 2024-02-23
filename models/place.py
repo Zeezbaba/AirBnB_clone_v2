d114893e6c268296df0eea7fe42e1095e21a17d7
Zeezbaba <Azeez_okeleji>
2024-02-22 17:15:52 -0800

    DBStorage - Review

--- models/place.py
+++ models/place.py
@@ -19,3 +19,28 @@ class Place(BaseModel, Base):
     latitude = Column(Float, default=0, nullable=True)
     longitude = Column(Float, default=0, nullable=True)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', backref="place",
+                               cascade="all, delete-orphan")
+        amenities = relationship('Amenity',
+                                 secondary='place_amenity',
+                                 backref='places', viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """Getter attribute in case of file storage"""
+            return [review for review in models.storage.all(Review)
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter attribute in case of file storage"""
+            return [amenity for amenity in models.storage.all(Amenity)
+                    if amenity.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter method for amenities"""
+            if (type(obj) == Amenity):
+                self.amenity_ids.append(obj.id)
