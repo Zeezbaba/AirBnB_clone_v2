b6f867c3aa48d0d23139d40c0997603c85b026b7
Zeezbaba <Azeez_okeleji>
2024-03-06 09:21:19 -0800

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static.
+# Install Nginx if it not already installed
+if ! command -v nginx &> /dev/null
+then
+	sudo apt-get -y update
+	sude apt-get -y install nginx
+fi
+
+# creating the required folders
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+# create a fake HTML file
+sudo echo -e "<html>
+	<head>
+	</head>
+	<body>
+	  Hello Holberton!
+	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+# Giving ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# restart Nginx
+sudo service nginx restart
