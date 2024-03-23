4733d3d8607820ae037390c9b2caa154ff3a048e
Zeezbaba <Azeez_okeleji>
2024-03-23 06:37:04 -0700

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """return Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def Hbnb():
+    """Return HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def CText(text):
+    """return C and text value"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
